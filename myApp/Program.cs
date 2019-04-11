121ec9df412cc4bd80a04114df559251e04931e1
pedroromn <pedroromn@gmail.com>
2019-04-11 09:23:30 -0500

    11/04/2019 09:23

---
+++ myApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+
+            var names = new List<string> {"Pedro", "Anny"};
+            foreach (string name in names){
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
