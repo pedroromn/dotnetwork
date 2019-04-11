dbd5cae210034a7419dc0e66f7b3733178c1ccab
pedroromn <pedroromn@gmail.com>
2019-04-11 10:51:30 -0500

    11/04/2019 10:51

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -9,11 +9,58 @@ namespace myApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("The current time is " + DateTime.Now);
-
+            /*
             var names = new List<string> {"Pedro", "Anny"};
-            foreach (string name in names){
+            foreach (var name in names){
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            //names.Remove("");
+            foreach (var name in names){
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine($"My name is {names[0]}.");
+            string value = $"I've added {names[2]} and {names[3]} to the list.";
+            Console.WriteLine(value);
+
+            value = $"The list has {names.Count} people in it";
+            Console.WriteLine(value);
+
+            var index = names.IndexOf("Bill");
+            if (index != -1)
+            Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            var notFound = names.IndexOf("Not Found");
+            Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
+
+            names.Sort();
+            foreach (var name in names)
+            {
+            Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            index = names.IndexOf("Bill");
+            if (index != -1)
+            Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            */
+
+            var fibonacciNumbers = new List<int> {1, 1};
+            
+            var end = 20;
+
+            for(int i = 3; i <= end; i++){
+              var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+              var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];  
+              fibonacciNumbers.Add(previous + previous2);                        
+            } 
+
+            foreach(var item in fibonacciNumbers)
+                Console.WriteLine(item);    
         }
     }
 }
